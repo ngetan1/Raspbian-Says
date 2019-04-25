77c5adeee05d8852805ad93ad2a45ed9ab835680
ngetan1 <43097300+ngetan1@users.noreply.github.com>
2019-04-24 23:07:45 -0400

    added button

--- app/src/main/java/com/example/raspbiansays/MainActivity.java
+++ app/src/main/java/com/example/raspbiansays/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.raspbiansays;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1, btn2, btn3, btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn3);
+        btn1.setBackgroundColor(Color.RED);
+        btn2.setBackgroundColor(Color.BLUE);
+        btn3.setBackgroundColor(Color.MAGENTA);
+        btn4.setBackgroundColor(Color.GREEN);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send(1);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send(2);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send(3);
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send(4);
+            }
+        });
+
+    }
+
+    public void send(int num){
+
     }
 }
